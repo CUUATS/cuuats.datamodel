a62290d4f7991a7f0cf2921a80ac49c1fef055fa
Matt Yoder <myoder@ccrpc.org>
2015-08-28 13:38:34 -0500

    Improved handling of class names and modules.

--- cuuats/datamodel/factory.py
+++ cuuats/datamodel/factory.py
@@ -1,10 +1,11 @@
+import inspect
 from cuuats.datamodel.features import BaseFeature
 from cuuats.datamodel.fields import BlobField, GeometryField, GlobalIDField, \
     OIDField, StringField, NumericField
 
 
-def feature_class_factory(name, source, register=True, exclude=[],
-                          follow_relationships=True):
+def feature_class_factory(fc_name, source, register=True, exclude=[],
+                          follow_relationships=True, class_name=None):
     """
     Create a feature class by introspecting the data source.
     """
@@ -12,12 +13,18 @@ def feature_class_factory(name, source, register=True, exclude=[],
     class Feature(BaseFeature):
         pass
 
-    Feature.__name__ = name
+    # Set the name of the feature class.
+    Feature.__name__ = class_name or fc_name
 
+    # Set the module of the feature class to the module of the caller.
+    frame = inspect.stack()[1]
+    Feature.__module__ = inspect.getmodule(frame[0])
+
+    # Add fields to the feature class.
     # TODO: Handle Date and Raster field types.
     # TODO: Handle field names that conflict with Feature attributes
     # and methods.
-    for (db_name, db_field) in source.get_layer_fields(name).items():
+    for (db_name, db_field) in source.get_layer_fields(fc_name).items():
         field = {
             'Blob': BlobField,
             'Geometry': GeometryField,
@@ -35,6 +42,6 @@ def feature_class_factory(name, source, register=True, exclude=[],
         pass
 
     if register:
-        Feature.register(source, name)
+        Feature.register(source, fc_name)
 
     return Feature
