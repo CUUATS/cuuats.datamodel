a65817a7fe735243dffec8ada28ae33fb7b64a9a
Matt Yoder <myoder@ccrpc.org>
2015-09-24 11:53:23 -0500

    Export the require_registration decorator.

--- cuuats/datamodel/__init__.py
+++ cuuats/datamodel/__init__.py
@@ -2,7 +2,7 @@ from cuuats.datamodel.workspaces import WorkspaceManager, Workspace
 from cuuats.datamodel.fields import BaseField, OIDField, GlobalIDField, \
     GeometryField, StringField, NumericField, CalculatedField, ScaleField, \
     WeightsField, MethodField, ForeignKey
-from cuuats.datamodel.features import BaseFeature
+from cuuats.datamodel.features import BaseFeature, require_registration
 from cuuats.datamodel.scales import BaseScale, BreaksScale, DictScale, \
     StaticScale
 from cuuats.datamodel.factory import feature_class_factory
